cbad30c904f076a428b799d45dd5224091e45c36
ravi.kant <ravi.kant@magicedtech.com>
2021-04-20 20:27:38 +0530

    routes and user model added

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const moongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await moongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err.message);
+    // Exit proccess with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const usersRoutes = require('./routes/api/users');
+const authRoutes = require('./routes/api/auth');
+const postRoutes = require('./routes/api/post');
+const profileRoutes = require('./routes/api/profile');
 
 const app = express();
 
+// connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API RUNNING'));
+
+// Define routes
+app.use('/api/users', usersRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/profile', profileRoutes)
+app.use('/api/posts', postRoutes)
 
 const PORT = process.env.PORT || 5000;
 
