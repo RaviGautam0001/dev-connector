95f2065182269b8d4f9e0f1694c015be64e55424
ravi.kant <ravi.kant@magicedtech.com>
2021-04-22 10:56:28 +0530

    user registred

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await moongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+    await moongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     console.log(err.message);
